73ca014d7b9cd4379e81c91056303dd845bbeafc
Alaricode <antonlarichev@gmail.com>
2022-05-01 18:20:38 +0300

    Код - Настраиваем nx monorepo

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule],
 })
 export class AppModule {}
